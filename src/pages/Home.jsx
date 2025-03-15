e99ad0d3ec520c92a0b3087208eff56c2a1bf7cb
JEPerezGomez <je.perezgomez@yahoo.com>
2025-03-15 12:10:13 -0400

    Estructura Inicial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Bienvenido a la Página Home</h1>
+            <p>Esta es la página principal de nuestro proyecto.</p>
+        </div>
+    );
+};
+
+export default Home;
